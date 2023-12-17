2af4fb85a71485f6ccef4589c863ac9123d92238
g.benattia <g.benattia@student.isartdigital.com>
2023-12-17 18:23:06 +0100

    Cleaning code + Replacing hard-coded values with const floats

--- Assets/Scripts/Observator.cs
+++ Assets/Scripts/Observator.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Observator : MonoBehaviour
 {
     [SerializeField] public bool InDoor;
-    [HideInInspector] public float PerceivedVolume;
-    [HideInInspector] public float PerceivedFrequency;
-    [HideInInspector] public float LastDistanceFromVehicule;
-    [HideInInspector] public bool IsLowPitched;
+    /*[HideInInspector]*/ public float PerceivedVolume;
+    /*[HideInInspector]*/ public float PerceivedFrequency;
+    /*[HideInInspector]*/ public float LastDistanceFromVehicule;
+    /*[HideInInspector]*/ public bool IsLowPitched;
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Simulation.cs
+++ Assets/Scripts/Simulation.cs
@@ -9,10 +9,12 @@ public class Simulation : MonoBehaviour
     [SerializeField] private Vehicule m_Car;
     [SerializeField] private Observator[] m_Observators = null;
 
+    private const float m_AirImpedance = 414f;
+    private const float m_WaveCelerityInAir = 343f;
+
     [SerializeField] public float Transmission;
 
     private const float m_I0 = 1.1e-12f;
-
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +31,7 @@ public class Simulation : MonoBehaviour
 
     float GetTransmission()
     {
-        float R = ((414f - m_Inputs.AcousticWaveImpedance) / (414f + m_Inputs.AcousticWaveImpedance)) * ((414f - m_Inputs.AcousticWaveImpedance) / (414f + m_Inputs.AcousticWaveImpedance));
+        float R = ((m_AirImpedance - m_Inputs.AcousticWaveImpedance) / (m_AirImpedance + m_Inputs.AcousticWaveImpedance)) * ((m_AirImpedance - m_Inputs.AcousticWaveImpedance) / (m_AirImpedance + m_Inputs.AcousticWaveImpedance));
 
         return (1f - R);
     }
@@ -38,7 +40,7 @@ public class Simulation : MonoBehaviour
     {
         float Radius = Vector3.Distance(Pos, m_Car.transform.position);
 
-        float I = 100f / (4f * Mathf.PI * (Radius*Radius));
+        float I = m_Inputs.AcousticWavePower / (4f * Mathf.PI * (Radius*Radius));
 
         return I;
     }
@@ -56,9 +58,9 @@ public class Simulation : MonoBehaviour
     void GetPerceivedFrequency(Observator Observator)
     {
         if (IsCarGettingCloser(m_Car, Observator))
-            Observator.PerceivedFrequency = m_Inputs.Frequency * (343f / (343f - m_Car.Speed)); // if f' < f low pitched
+            Observator.PerceivedFrequency = m_Inputs.Frequency * (m_WaveCelerityInAir / (m_WaveCelerityInAir - m_Car.Speed)); // if f' < f low pitched
         else
-            Observator.PerceivedFrequency = m_Inputs.Frequency * (343f / (343f + m_Car.Speed)); // if f' > f high pitched
+            Observator.PerceivedFrequency = m_Inputs.Frequency * (m_WaveCelerityInAir / (m_WaveCelerityInAir + m_Car.Speed)); // if f' > f high pitched
     }
 
     bool IsCarGettingCloser(Vehicule Car, Observator Observator)
@@ -87,23 +89,23 @@ public class Simulation : MonoBehaviour
     {
         Transmission = GetTransmission();
 
-        //foreach (var observator in m_Observators)
-        //{
-        //    GetVolume(observator);
-        //    GetPerceivedFrequency(observator);
-        //    GetPitchType(observator);
+        foreach (var observator in m_Observators)
+        {
+            GetVolume(observator);
+            GetPerceivedFrequency(observator);
+            GetPitchType(observator);
+        }
+
+        //GetPerceivedFrequency(m_Observators[0]);
+        //GetPitchType(m_Observators[0]);
+
+        //if (m_Observators[0].IsLowPitched) {
+        //    Debug.Log(m_Observators[0].PerceivedFrequency);
+        //    Debug.Log("PLUS GRAVE");
         //}
-
-        GetPerceivedFrequency(m_Observators[0]);
-        GetPitchType(m_Observators[0]);
-
-        if (m_Observators[0].IsLowPitched) {
-            Debug.Log(m_Observators[0].PerceivedFrequency);
-            Debug.Log("PLUS GRAVE");
-        }
-        else {
-            Debug.Log(m_Observators[0].PerceivedFrequency);
-            Debug.Log("PLUS AIGU");
-        }
+        //else {
+        //    Debug.Log(m_Observators[0].PerceivedFrequency);
+        //    Debug.Log("PLUS AIGU");
+        //}
     }
 }
