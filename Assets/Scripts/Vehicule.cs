4a9829f05253036d2fdd615dc8584ac0a9087e64
g.benattia <g.benattia@student.isartdigital.com>
2023-12-18 01:05:47 +0100

    Adding details to the scene

--- Assets/Scripts/Vehicule.cs
+++ Assets/Scripts/Vehicule.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static System.TimeZoneInfo;
 
 public class Vehicule : MonoBehaviour
 {
     [HideInInspector] public float Speed = 0f;
     [SerializeField] public AcousticWaveTransmitter WaveTransmitter = null;
+    [SerializeField] private Light m_Light;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float minIntensity = 0f; 
+    public float maxIntensity = 300f;
+    public float frequency = 1f;
 
-    // Update is called once per frame
     void Update()
     {
+        float time = Time.time * frequency;
+        float intensity = Mathf.Lerp(minIntensity, maxIntensity, Mathf.Abs(Mathf.Sin(time)));
+
+        m_Light.intensity = intensity;
+
         transform.position += new Vector3(Speed * -1f * Time.deltaTime, 0f, 0f);
     }
 }
